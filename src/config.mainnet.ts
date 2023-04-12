a00e790595508aee9fa2f97c04e52694a3e62a41
NevilleNguyen <quangnguyen.dfse@gmail.com>
2023-04-12 09:24:21 +0700

    config delegation contract

--- src/config.mainnet.ts
+++ src/config.mainnet.ts
@@ -52,7 +52,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://gateway.multiversx.com',
   explorerAddress: 'https://explorer.multiversx.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq3lllllsw2eke2'
 };
 
 export const delegationContractData: DelegationContractType[] = [
